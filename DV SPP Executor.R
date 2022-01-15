b051add00f399aa5762513c9f5ceceac3993f639
dvanjani24 <97678601+dvanjani24@users.noreply.github.com>
2022-01-15 17:48:59 -0500

    Update DV SPP Executor.R

--- DV SPP Executor.R
+++ DV SPP Executor.R
@@ -1,17 +1,18 @@
-# Clear
 rm(list = ls())
 current_working_dir <-
   dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(current_working_dir)
 
 # Libraries
-libraries = c(
+library = c(
   "shiny","tidyverse","readr","plotly","shinydashoard","rsconnect","rvest",
   "data.table","BatchGetSymbols","dplyr","googlesheets4","DT","tableHTML",
   "qdapRegex","quantmod","shinythemes","stringi","xlsx","xts","stringr",
   "forcats","lubridate","PerformanceAnalytics","forecast","tseries","fGarch",
   "rugarch","tsfknn","MLmetrics", "finreportr", "devtools", "XBRL", "readxl",
   "MASS", "leaps", "caret", "MuMIn", "openxlsx")
+
+libraries <- c("readxl", "openxlsx", "lubridate", "data.table", "dplyr")
 suppressWarnings(lapply(libraries, require, character.only = TRUE))
 
 # Full-model examination
